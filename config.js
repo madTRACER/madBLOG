b42aa8756803a6bee353f7d3d3289190820d4908
madTRACER <madtracer00@gmail.com>
2020-10-17 20:09:21 +0600

    modified config

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://madtracer.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'madTRACER | cyberImplantation',
+  subtitle: '',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-103653591-1',
   useKatex: false,
   menu: [
     {
@@ -16,30 +16,26 @@ module.exports = {
       path: '/'
     },
     {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: 'Who is',
+      path: '/pages/whois'
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Tim Koval',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Embedded & AI/ML Engineer',
     contacts: {
       email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      facebook: '',
+      telegram: 'madTRACER',
+      twitter: '',
+      github: 'madTRACER',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: 'madtracer',
+      instagram: '_madtracer_',
       line: '',
-      gitlab: '',
+      gitlab: 'madTRACER',
       weibo: '',
       codepen: '',
       youtube: '',
