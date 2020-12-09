20f6e6d887d49692c4050ae87390505fd0ef8602
madTRACER <madtracer00@gmail.com>
2020-12-09 21:24:21 +0300

    changed website url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://madtracer.netlify.com',
+  url: 'https://madtracer.com',
   pathPrefix: '/',
   title: 'madTRACER | cyberImplantation',
   subtitle: '',
